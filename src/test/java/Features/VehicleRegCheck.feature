11376c51d3728b517ddf0035b0825c3de5f10cb7
goldxfisch <vikas.bhatia.work@outlook.com>
2018-05-15 17:16:04 +0100

    Logging and Unit Test Class for ScanDirectory(possibly using Mocks)

--- src/test/java/Features/VehicleRegCheck.feature
+++ src/test/java/Features/VehicleRegCheck.feature
@@ -8,17 +8,4 @@ Feature: DVLA Checks
     Then the user is displayed vehicle detail "<registration>" , "<make>", "<colour>"
     Examples:
     | registration | make | colour |
-    | KV60 XDV| XXXX| BLACK |
-| APB10 PB| ZZZZ| RED |
-| DA11 PZM| AUDI| BLUE |
-| CE11 XUA| HONDA| RED |
-| KV65 XDV| XXXX| BLACK |
-| APB13 PB| ZZZZ| RED |
-| DA10 PZM| AUDI| BLUE |
-| CE51 XUA| HONDA| RED |
-| DA00 PZM| AUDI| BLUE |
-| CE00 XUA| HONDA| RED |
-| APB13 PB| XXXX| YELLOW |
-| CE51 XUA| HONDA| RED |
-| KV65 XDV| BMW| RED |
 | DA10 PZM| AUDI| BLUE |
---
+++ src/test/java/Runner/runUnitTestServiceLayer.java
@@ -0,0 +1,29 @@
+package Runner;
+
+import ServiceLayer.ScanDirectory;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pojo.FileDetails;
+
+import java.io.IOException;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class runUnitTestServiceLayer {
+    final static Logger logger = LoggerFactory.getLogger(runUnitTestServiceLayer.class);
+
+    private List<FileDetails> ListOfFiles;
+
+    @Parameterized.Parameters(name = "{index}: File - {0}")
+    public static Iterable<? extends Object> data() throws IOException {
+        List<FileDetails> listOfFiles =  new ScanDirectory().crawlDirectory();
+        return listOfFiles;
+        }
+
+
+
+
+
+}
